68ccfd8f706c9a9a1be23153eb6a3b2930da18a9
avaldemoro <astervaldemoro@gmail.com>
2020-09-10 02:56:06 -0400

    initial nav bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { FaHome, FaLaptopCode, FaRegAddressCard } from 'react-icons/fa';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse: "" };
+    }
+    
+    callAPI() {
+        fetch("/testAPI")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res }));
+    }
+    
+    componentDidMount() {
+        this.callAPI();
+    }
+
+    render() {
+        return (
+            <>
+                <Navbar className="Navbar" variant="dark">
+                    <Navbar.Brand className="NavbarLogo" href="#home">AV</Navbar.Brand>
+                    <Nav className="NavbarItems">
+                        <Nav.Link className="NavItem" href="#deets"><FaHome/></Nav.Link>
+                        <Nav.Link className="NavItem" href="#deets"><FaLaptopCode/></Nav.Link>
+                        <Nav.Link className="NavItem" eventKey={3} href="#memes"><FaRegAddressCard/></Nav.Link>
+                    </Nav>
+                </Navbar>
+                <Container>
+                    <div>
+                        <p className="App-intro">{this.state.apiResponse}</p>
+                    </div>
+                </Container>
+            </>
+        );
+    }   
 }
 
 export default App;
