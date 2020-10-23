94f953fd1d588f79b5f2f9f19a6f2f56cf788664
avaldemoro <astervaldemoro@gmail.com>
2020-10-22 22:48:28 -0400

    sectioned off different parts of website to their own functions

---
+++ src/sections/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function Footer(props) {
+    return (
+        <footer id="footer" class="wrapper style1-alt">
+            <div class="inner">
+                <ul class="menu">
+                    <li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                </ul>
+            </div>
+        </footer>
+    );
+}
